b07b3e8cd5fed3b5ba9f35f192f494f495b349e9
BuildTools <unconfigured@null.spigotmc.org>
2020-05-15 16:14:19 +0300

    testing ascii

--- reversy.cpp
+++ reversy.cpp
@@ -18,7 +18,16 @@ bool bCheckKeyState = false;
 
 auto tp1 = chrono::system_clock::now();
 auto tp2 = chrono::system_clock::now();
+
 short nShade = 0x0;
+/*
+	0x1 = black units;
+	0x2 = white units;
+	0x48 = 0, then: numbers;
+	0x65 = A, then: uppercase letters;
+	0x97 = a, then: lowercase letters;
+
+*/
 
 wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight + 1];
 
@@ -37,12 +46,12 @@ int main() {
 		if (fElapsedTime > fMaxElapsedTime) fMaxElapsedTime = fElapsedTime;
 		
 		TCHAR title[MAX_PATH];
-		StringCchPrintf(title, MAX_PATH, TEXT("FPS=%3.2f FPS_Max=%3.2f FPS_Min=%3.2f Hesh=%1.0f"), 1.0f / fElapsedTime, 1.0f / fMinElapsedTime, 1.0f / fMaxElapsedTime, (float)nShade);
+		StringCchPrintf(title, MAX_PATH, TEXT("FPS = %3.2f | FPS_Max = %3.2f | FPS_Min = %3.2f | ASCII Code = %1.0f"), 1.0f / fElapsedTime, 1.0f / fMinElapsedTime, 1.0f / fMaxElapsedTime, (float)nShade);
 		SetConsoleTitle(title);
 		
 		if (GetAsyncKeyState(0x41) & 0x8000 || GetAsyncKeyState(0x44) & 0x8000) {
-			if (GetAsyncKeyState(0x41) & 0x8000 && !bCheckKeyState) nShade += 1;
-			if (GetAsyncKeyState(0x44) & 0x8000 && !bCheckKeyState) nShade -= 1;
+			if (GetAsyncKeyState(0x41) & 0x8000 && !bCheckKeyState) nShade -= 1;
+			if (GetAsyncKeyState(0x44) & 0x8000 && !bCheckKeyState) nShade += 1;
 			bCheckKeyState = true;
 		}
 		else bCheckKeyState = false;
